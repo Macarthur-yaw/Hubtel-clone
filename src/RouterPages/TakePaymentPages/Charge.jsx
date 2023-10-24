836fe526efad98a38083d4b11ba11bbe15d1a05d
Kevin <arthurkevin1260@gmail.com>
2023-10-24 21:55:51 +0100

    added the interPage

--- src/RouterPages/TakePaymentPages/Charge.jsx
+++ src/RouterPages/TakePaymentPages/Charge.jsx
@@ -1,29 +1,144 @@
 import backgroundPicture from '../../assets/bg-chargecustomers.webp'
+import {FaApple,FaAndroid} from 'react-icons/fa'
+import posPicture from '../../assets/pos-app.webp'
+import posDevice from '../../assets/pos-device.webp'
+import posDevice2 from '../../assets/pos-device-white.webp'
+import posReceipt from '../../assets/pos-reciept.webp'
+import majorPayments from '../../assets/major-payments (1).webp'
+
 const Charge = () => {
     return ( 
-        <div className='flex flex-row pt-20  border-2'>
-             
-             <div className='w-[58%]'>
+        <div>
 
-                <h1>
+        <div className='grid grid-cols-2 pt-20 place-items-center  '>
+             
+             <div className='w-[58%] flex flex-col gap-4'>
+
+                <h1 className='text-[25px] font-bold-200 '>
                 Take card & mobile money payments at your pay point
                 </h1>
 
-                <h3>
+                <h3 className='text-[20px] font-semibold'>
                 Join thousands of smart businesses – from savvy start-ups to expanding enterprises – using a Hubtel card machine.
 
                 </h3>
 
-                <button className='text-[#01c7b1] bg-[#01c7b1] border-[#01c7b1] border-2 text-white p-2 rounded-md'>
+                <button className='text-[#01c7b1] bg-[#01c7b1] border-[#01c7b1] border-2 text-white p-2 rounded-md w-[30%]'>
                     GET STARTED
                 </button>
              </div>
 
              <div>
-<img src={backgroundPicture} alt='a man and a woman' width='50%'/>
+<img src={backgroundPicture} alt='a man and a woman' className='max-w-[75%]'/>
              </div>
 
+             </div>
+        
+        <div className='grid grid-cols-2 pt-[13%] place-items-center items-center justify-evenly p-4'>
+<div className='flex flex-col'>
+            <div  className='flex flex-col'>
+            <h1 className='font-semibold text-[25px]'>
+                Sell like a big business
+            </h1>
+            <h3 className='w-[70%] pb-2 border-b-[1px] border-b-[#878b90]'>
+            Capture sales, generate invoices, track payments from your smartphone.
+
+Activate your Hubtel Sales app to sell your store items and take payments from all customers.
+            </h3>
+            </div>
+            <div id='icons'>
+<p>Get the app</p>
+
+<div id='subIcons' className='flex flex-row'>
+<FaAndroid className='text-[35px]'/>
+<FaApple className='text-[35px]'/>
+
+</div>       </div>
+
+</div>       
+
+<div>
+<img src={posPicture} width='80%'/>
+            </div>
+
         </div>
+
+        <div className='bg-[#f8f9fb] pt-20 flex flex-col gap-6'>
+            <h1 className='text-center text-[25px] font-bold'>
+                Pick the one that suits your needs
+            </h1>
+
+<div className='flex  flex-row justify-evenly p-6'>
+
+<div id='cardOne' className='border-2 bg-white border-white shadow-sm p-2 rounded-lg w-[30%]'>
+
+<img src={posDevice} />
+<h1 className='font-bold text-center text-[20px]'>
+Miura Card Reader
+</h1>
+
+<h3 className='text-center'>
+Pairs over Bluetooth with your Android or iOS smartphone to suit all kinds of scenarios.
+</h3>
+
+<h5 className='text-center text-[#878b90] text-[12px]'>
+Perfect for businesses on the go.
+</h5>
+</div>
+
+<div id='cardOne' className='border-2 bg-white border-white shadow-sm p-2 rounded-lg w-[30%]'>
+
+<img src={posDevice2}/>
+<h1 className='font-bold text-center text-[20px]'>
+Miura Card Reader
+</h1>
+
+<h3 className='text-center'>
+Pairs over Bluetooth with your Android or iOS smartphone to suit all kinds of scenarios.
+</h3>
+
+<h5 className='text-center text-[#878b90] text-[12px]'>
+Perfect for businesses on the go.
+</h5>
+</div>
+
+
+
+</div>
+        </div>
+
+<div>
+
+<div className='flex flex-row pt-20 p-4'>
+    <div>
+    <h1 className='font-bold text-[20px]'> 
+    Get paid instantly; no settlement wait
+    </h1>
+
+    <h3 className='w-[60%]'>
+    Hubtel is the only payments provider that doesn’t hold your money after a payment is completed.
+
+Your mobile money or bank account is credited the minute a payment to you is completed.
+    </h3>
+    </div>
+
+
+<div className=''>
+    <img src={posReceipt}/>
+</div>
+</div>
+    </div>
+
+<div className='flex flex-col gap-6'>
+    <h1 className='font-bold text-[30px] text-center'>Accept all other payments</h1>
+
+<div className='mx-auto w-[70%]'>
+<img src={majorPayments} className=' '/>
+
+</div>
+</div>
+
+         </div>
      );
 }
  
--- src/RouterPages/TakePaymentPages/InterPage.jsx
+++ src/RouterPages/TakePaymentPages/InterPage.jsx
@@ -1,7 +1,139 @@
+import devPortal from '../../assets/dev-portal.webp'
+import moveMoney from '../../assets/move-monoey.png'
+import moveMoney1 from '../../assets/move-monoey (1).png'
+import majorPayment from '../../assets/major-payments-v.webp'
+import checkIcon from '../../assets/feather-check-circle.png'
+import insightPic from '../../assets/insights.png'
+import payLoard from '../../assets/payload.png'
+const pageContent=[
+    { 
+        id:1,
+        title:"Connect to the most reliable payment gateway in Ghana",
+        content:"Offer your buyers the most popular payment methods — GHQR, MTN Mobile Money, Vodafone Cash, AirtelTigo, Visa, Mastercard — in a single integration."
+    }
+    ,{
+        id:2,
+        title:"Minimise payment friction with direct gateway integration",
+        content:"Take payments directly on your website by integrating Hubtel’s Direct Merchant APIs to your sales process.(Available on request. Subject to additional connectivity requirements and service terms)."
+    },{
+        id:3,
+        title:"Generate new sales by attracting younger customers",
+        content:"Tap into the millennial and gen-Z market. 83% of users on Hubtel are below the age of 40, with a high return rate and stronger loyalty for brands that process services and payments online."
+    }
+]
 const InterPage = () => {
     return ( 
-        <div>
-            inter page
+        <div className='pt-20 '>
+
+          <div className='p-10'>  
+            <div className='text-center flex flex-col gap-4 items-center'>
+                <h1 className='text-[25px] font-bold '>
+                Integrate Hubtel’s checkout to get paid hourly to instantly.
+                </h1>
+
+                <h3 className='text-[20px] font-semibold'>
+                Don’t wait 2 to 3 days to receive payments from your online checkout
+                </h3>
+
+                <button className='text-[#01c7b1] bg-[#01c7b1] border-[#01c7b1] border-2 text-white p-2 rounded-md w-[10%]'>GET STARTED </button>
+            </div>
+
+            <div>
+                <img src={devPortal} className='w-[60%] mx-auto'/>
+            </div>
+            </div>
+
+            <div className='grid grid-cols-2 place-item-center p-4 items-center '>
+
+                <div>
+                <h1 className='font-bold text-[25px] w-[75%]'>
+                Move your money to any bank account or mobile
+                
+                 money wallet
+                </h1>
+
+                <h3 className='w-[60%] text-[20px]'>
+                Hubtel is the only online payment gateway with direct access to Ghana’s interbank payment switch.
+
+Meaning your money is safe, and can be transferred instantly to any bank account or mobile money wallet.
+                </h3>
+                </div>
+<div>
+<img src={moveMoney} alt='a box'/>
+
+
+    
+</div>
+            </div>
+
+            <div className='bg-[#eff8fc] p-4'>
+<div className='flex flex-col  gap-2'>
+    <h1 className='text-[20px] font-semibold '>
+    Let buyers pay how they want
+    </h1>
+
+    <h3 className='text-[18px] font-semibold w-[70%]'>
+    Offer your buyers the most popular payment methods — GHQR, MTN Mobile Money, Vodafone Cash, AirtelTigo, Visa, Mastercard — in a single integration.
+
+    </h3>
+</div>
+
+<div className='flex flex-row'>
+
+    <div className='flex flex-col gap-4 '>
+{pageContent.map((content)=>(
+    <div key={content.id} >
+
+        <div className='flex flex-row gap-2'>
+
+           <img src={checkIcon}/> 
+      <h1 className='text-[20px] font-semibold'>
+      {content.title}
+        </h1> 
+</div>
+        <h3 className='w-[85%]'>
+        {content.content}
+        
+        </h3>
+        </div>
+))}
+</div>
+
+<img src={majorPayment} width='13%'/>
+     </div>       </div>
+ 
+ <div className='flex flex-row items-center justify-center'>
+
+<img src={insightPic} width='40%'/>
+    <div>
+        <h1 className='text-[20px] font-semibold'>
+        Dive deeper into your payment and customer insights
+        </h1>
+
+        <h3 className='text-[18px] font-normal'>Understand what’s happening with your sales by combining performance data on:</h3>
+
+        <ul className='list-disc px-6'>
+            <li>
+            Payments
+            </li>
+<li>Active customers</li>
+<li>New customers</li>
+<li>Dormant customers</li>
+<li>Busiest day/times</li>
+<li>Preferred payment methods
+            </li>
+        </ul>
+    </div>
+ </div>
+
+ <div className='pb-20 bg-[#eff8fc] pt-6 flex flex-col items-center gap-4'>
+    <img src={payLoard} alt='a payload' className='mx-auto rounded-md  w-[60%] h-[100%]'/>
+ <button className='border-2 p-2 rounded-md w-[30%]'>
+    START INTEGRATING
+ </button>
+ 
+ </div>
+ 
         </div>
      );
 }
