2e669e001a033819d85f63749137cb6c312a7485
Kevin <arthurkevin1260@gmail.com>
2023-11-08 17:32:38 +0000

    still building

--- src/RouterPages/ServePages/Restaurant.jsx
+++ src/RouterPages/ServePages/Restaurant.jsx
@@ -1,9 +1,153 @@
+import serveCustomers from '../../assets/serve-customers.webp'
+import salesIcon from '../../assets//sales.png'
+import customersIcon from  '../../assets/customers.png'
+import truckIcon from '../../assets/Order.png'
+import { useState } from 'react'
+import orderDetails from '../../assets/Order-details.webp'
+import livePic from '../../assets/live-activity (1).webp'
+import roundOne from '../../assets/round1.png'
+import roundTwo from '../../assets/round2.png'
+import roundThree from '../../assets/round3.png'
+import liveActivity from '../../assets/live-activity.webp'
 const Restaurant = () => {
+    const[image,setImage]=useState(null)
+    function pictureDisplay(imageUrl){
+        setImage(imageUrl)
+    }
     return (
-        <div>
-            this is a restaurant 
+        <div className='py-20 '>
+            <div className='flex bg-gray-100 py-10 flex-row items-center justify-center'>
+                <span className='w-[40%] flex flex-col gap-2'>
+                    <h1 className='text-[25px] font-semibold'>
+                    Serve your meals to hungry
+                    <br/>
+                     customers nearby
+                    </h1>
+
+                    <h2 className='text-[20px]'>
+                    Reach more customers with your food servings. Restaurants on Hubtel see a sales boost of up to 42% within the first 90 days.
+                    </h2>
+
+                    <button className='text-[13px] font-semibold border-[#01c7b1] border-2 bg-[#01c7b1] p-2 text-white rounded-md w-[40%]'>
+                        ADD MY RESTAURANT
+                    </button>
+                </span>
+
+                <img src={serveCustomers}   className='w-[35%]'/>
+            </div>
+
+            <div className='py-20 '>
+                <h1 className='font-bold text-[25px] px-6'>Modernise and grow your restaurant.</h1>
+              <div className=' p-4 flex flex-row justify-center'>
+              <div className='flex flex-col p-2 gap-2'>
+                <button
+                onClick={()=>pictureDisplay(orderDetails)}
+                className='flex flex-col gap-[4px] w-[100%] p-2 rounded-md border-[1px] focus:border-[#3159f5] focus:bg-[#f2f5ff]'>
+<img src={salesIcon}/>
+<h1 className='font-semibold text-[20px]'>Increase sales(orders)</h1>
+<h3 className='text-left'>Grow your kitchen orders to keep it busy throughout the day.</h3>
+                </button>
+
+                <button onClick={()=>pictureDisplay(livePic)}
+                 className='flex border-[1px] gap-[4px] rounded-md p-2    flex-col w-[100%] focus:border-[#3159f5] focus:bg-[#f2f5ff]'>
+<img src={customersIcon}/>
+<h1 className='font-semibold text-[20px]'>Reach more Customers</h1>
+<h3 className='text-left'>Hubtel's marketing will help you attract new local customers and keep them coming back for more.
+
+</h3>
+                </button>
+
+<button onClick={()=>pictureDisplay(orderDetails)}
+ className='flex flex-col w-[100%] border-[1px] gap-[4px] rounded-md p-2 focus:border-[#3159f5] focus:bg-[#f2f5ff]'>
+<img src={truckIcon}/>
+<h1 className='font-semibold text-[20px] text-left'>Use our riders to deliver at no extra cost</h1>
+<h3 className='text-left'>
+    Hubtel's riders are trained,motivated, and equipped to deliver your meals professionally to customers on time.
+
+</h3>
+</button></div>
+<div className='border-[1px] w-[160%] border-[#3159f5] bg-[#f2f5ff]  rounded-md'>
+{image && <img src={image} className=''/>}
+</div>
+</div>    </div>
+
+<div className='flex flex-col gap-10 py-20 bg-[#f2fff5]'>
+
+<div>
+<h1 className='text-[25px] font-semibold text-center'>Manage your online orders with Hubtel</h1>
+<h2 className='text-center text-[20px]'>Fire up your kitchen for Hubtel orders in a few easy steps</h2>
+</div>
+
+<div className='flex flex-row gap-4'>
+<SectionCard imageUrl={roundOne} title='Come onboard' content='Tell us about your restaurant'/>
+<SectionCard imageUrl={roundTwo} title='Set up your restaurant on Hubtel' content='Upload a menu, schedule your photoshoot and if you need one we’ll ship your tablet.'/>
+<SectionCard imageUrl={roundThree} title='Start serving meals' content=' Sell,promote, track and manage your orders from the Hubtel Quick Commerce Portal on your smartphone,tablet or computer.'/>
+</div>
+
+</div>
+
+<div>
+<div className='flex flex-row justify-center gap-4 py-20'>
+
+<div className='border-2 border-gray-100 bg-gray-100 w-[40%] max-h-[780px] shadow-2xl rounded-md py-28'>
+<img src={liveActivity}/>
+
+</div>
+
+<div className='border-2 border-white  bg-white shadow-2xl rounded-sm p-4 max-h-[780px] w-[35%]'>
+<h1 className='font-semibold text-[23px] p-4 text-center'>
+Let's get you set up and ready for orders
+</h1>
+<form className='flex flex-col gap-[1px] mx-auto '>
+<span className='flex flex-col gap-[4px]'>
+<label className='text-[16px] font-semibold text-gray-600' htmlFor='Name'>Business Name:</label>
+
+<input className='border-[1px]  outline-none border-gray-300 w-[100%] p-[10px] rounded-sm' type='text' name='Name' placeholder='Eg.Rethink Restaurant'/>
+</span>
+<br/>
+<span className='flex flex-col gap-[4px]'><label className='text-gray-600 text-[16px] font-semibold' htmlFor='Location'>Location</label>
+<input className='border-[1px] outline-none border-gray-300 w-[100%] p-[10px] rounded-sm' type='text' name='Location' placeholder='Eg. Kokomlemle'/>
+</span>
+
+<br/>
+<span className='flex flex-col gap-[4px]'>
+<label className='text-[16px] font-semibold text-gray-600' htmlFor='City'>City</label>
+
+<input className='border-[1px] outline-none border-gray-300 w-[100%] p-[10px] rounded-sm' type='text' name='City' placeholder='Eg. Accra'/>
+</span>
+<br/>
+<span className='flex flex-col gap-[4px]'>
+<label className='text-[16px] font-semibold text-gray-600' htmlFor='Email'>Email</label>
+
+<input type='text' className='border-2 outline-none border-gray-300 w-[100%] p-[10px] rounded-sm' name='Email' placeholder='Eg. example@gmail.com'/>
+</span>
+<br/>
+<span className='flex flex-col gap-[4px]'>
+<label className='text-[16px] font-semibold text-gray-600' htmlFor='Phone'>Phone</label>
+
+<input type='text' className='border-2 outline-none border-gray-300 w-[100%] p-[10px] rounded-sm' name='Phone' placeholder='Eg. 0545938774'/>
+</span>
+<br/>
+
+
+<br/>
+<button className='bg-[#01c7b1] text-white border-2 border-[#01c7b1] p-2 font-bold w-[100%] text-[13px]  rounded-md'>ADD MY SHOP</button>
+</form>
+</div>
+
+</div>
+</div>
         </div>
       );
+}
+function SectionCard(props){
+    return(
+        <div className='flex flex-col gap-2 w-[35%]  items-center'>
+            <img src={props.imageUrl} className='w-[50px] '/>
+            <h1 className='text-[25px] font-semibold'>{props.title}</h1>
+            <h2 className='text-[18px] text-center'>{props.content}</h2>
+        </div>
+    )
 }
  
 export default Restaurant;
--- src/RouterPages/ServePages/Riders.jsx
+++ src/RouterPages/ServePages/Riders.jsx
@@ -1,25 +1,204 @@
 import riderPic from '../../assets/riders.webp'
+import {FaWhatsapp} from 'react-icons/fa'
+import num1 from '../../assets/Group_39451.png'
+import num2 from '../../assets/Group_39451 (1).png'
+import num3 from '../../assets/Group_39451 (2).png'
+import num4 from '../../assets/Group_39451 (3).png'
+import deliveryGuy from '../../assets/delivery-guy.png'
+import checkMark from '../../assets/feather-check-circle (1).png'
+import phoneOrder from '../../assets/mockuuups-iphone-3.webp'
+import bgMobileDelivery from '../../assets/bg-mobile-deliver-app.svg'
+import mockupOne from '../../assets/mockuuups-iphone-1.webp'
+import mockupTwo from '../../assets/mockuuups-iphone-2.webp'
+import roundOne from '../../assets/round1.png'
+import roundThree from '../../assets/round3.png'
+import roundTwo from '../../assets/round2.png'
+import withYou from '../../assets/with-you.webp'
+
 const Riders = () => {
+
     return (  
-        <div>
-            <div>
-                <span>
-                    <h1>
-                    Earn money delivering smiles to customers within your community
+        <div className='py-20'>
+            <div className='flex flex-row items-center bg-gray-100 py-10 justify-center gap-10'>
+                <span className='flex flex-col gap-2'>
+                    <h1 className='text-[25px] font-semibold'>
+                    Earn money delivering smiles to
+                    <br/>
+                     customers within your community
                     </h1>
 
-                    <h2>
-                    Riders on Hubtel are professionally equipped, adequately trained and paid weekly.
+                    <h2 className='text-[18px]'>
+                    Riders on Hubtel are professionally equipped, adequately 
+                    <br/>
+                    trained and paid weekly.
                     </h2>
 
-                    <button className='bg-[#01c7b1] text-white border-2 border-[#01c7b1] p-2 font-bold w-[100%] text-[13px]  rounded-md'>JOIN US</button>
+                    <button className='bg-[#01c7b1] text-white border-2 border-[#01c7b1] p-2 font-bold w-[30%] text-[13px]  rounded-md inline-flex gap-2 items-center justify-center'><FaWhatsapp/> JOIN US</button>
                 </span>
-                <img src={riderPic}/>
+                <img src={riderPic} className='w-[35%]'/>
 
                 
+            </div>
+
+            <div className='py-20'>
+                <h1 className='text-center font-bold text-[25px]'>
+                    How we work
+                </h1>
+
+                <div className='flex flex-row items-center gap-6  justify-center'>
+                    <div className='grid grid-rows-2 grid-flow-col gap-4'>
+<RiderCard Imageurl={num1} title='Chat with us from your own smartphone to see if you qualify'/>
+<RiderCard Imageurl={num2} title='Get trained and equipped at no extra cost'/>
+<RiderCard Imageurl={num3} title='Choose a suitable station, work plan & schedule'/>
+<RiderCard Imageurl={num4} title='Start delivering and start earning.'/>
+</div>
+              
+              <img src={deliveryGuy} className='rounded-md w-[30%]'/>
+
+                </div>
+            </div>
+
+            <div className='py-20 flex flex-row items-center justify-between'>
+<h1 className='font-semibold text-[28px] p-2 '>
+Make money on your own schedule with a 
+<br/>
+scooter, motorbike or bicycle.
+</h1>
+
+<div className='p-2'>
+    <div className='flex flex-row gap-2'>
+    <SectionCard  title='Work when you want to'/>
+    <SectionCard  title='Get paid weekly'/>
+    </div>
+    <br/>
+    <SectionCard  title='Hit your  weekly delivery target to earn bonuses'/>
+</div>
+            </div>
+
+            <div className=' py-10  border-2 bg-gray-100 border-gray-100 '>
+           <div className='flex flex-col gap-10 py-24 px-8'>
+                <span className='text-center'>
+                    <h1 className='font-semibold text-[28px]'>
+                        How the delivery app works.
+                    </h1>
+                    <h3 className='text-[22px]'>
+                    We call it Hubtel Careforce
+                    </h3>
+                </span>
+                <div className='flex flex-row w-[100%] justify-center'>
+                <img src={mockupOne} className='w-[500px] h-[700px]'/>
+<img src={mockupTwo} className='w-[500px] h-[700px]'/>
+<img src={phoneOrder} className='w-[500px] h-[700px]'/>
+</div>
+</div>
+
+<div className='flex flex-row p-2 justify-center gap-2'>
+    <div className='w-[40%]  p-2'>
+        <img src={roundOne}/>
+        <h1 className='font-semibold text-[23px]'>Accept and go</h1>
+        <h2>
+        Once you start work, we will send you orders from near you to accept.
+
+        </h2>
+    </div>
+
+    <div className='w-[40%]  p-2'>
+        <img src={roundTwo}/>
+        <h1 className='font-semibold text-[23px]'>Inspect and collect orders</h1>
+
+        <h2>Ride to retailers and restaurants to inspect,
+            
+             pay and pick up orders for delivery to customers. </h2>
+
+    </div>
+
+    <div className='w-[40%]  p-2'>
+        <img src={roundThree}/>
+        <h1 className='font-semibold text-[23px]'>Make a customer smile by delivering on time!</h1>
+
+        <h2>
+        Follow Google Maps directions to a customer’s door, meet the customer, take your delivery confirmation code and earn your commission.
+        </h2>
+    </div>
+
+
+</div>
+            </div>
+
+            <div className='py-20'>
+                <h1 className='text-center font-bold text-[25px]'>What you need to start delivering</h1>
+           
+           <div className='flex flex-row gap-2 p-6 '>
+           <div className='flex flex-col gap-2'>
+            <DeliverCard  title='Motorbike, Scooter, or Bicycle'/>
+            <DeliverCard  title='A valid driver’s licence and insurance (Bicycle excluded)'/>
+            <DeliverCard  title='Government issued ID with no criminal background'/>
+            <DeliverCard  title='Safety equipment (e.g. helmet, shoulder, elbow, knee guard, and boots)'/>
+           </div>
+
+           <div className='flex flex-col gap-2 '>
+            <DeliverCard title='Age 18+'/>
+            <DeliverCard title='Smartphone (with Android 6+ or iOS 13.6)'/>
+            <DeliverCard title='Proof of your right to work if not a citizen of the operating country'/>
+           </div>
+
+           </div>
+            </div>
+
+            <div className='flex flex-row items-center bg-gray-100 py-20'>
+
+<span className='flex flex-col gap-2 p-2 '>
+    <h1 className='text-[25px] font-semibold'>
+    We’re with you all the way
+    </h1>
+
+    <h2 className='text-[20px]'>
+    When you’re on the road, we’re with you – for help, advice and support, message or call us in the app.
+
+Your safety is our priority. In case of accidents or unforeseen incidents, we dispatch help to you to assist.
+
+It’s totally free and applies from the moment you go online.
+    </h2>
+
+    <button className='bg-[#01c7b1] text-white border-2 border-[#01c7b1] p-2 font-bold items-center text-[13px]  rounded-md inline-flex  gap-2 w-[70%]'><FaWhatsapp/> JOIN THE FASTEST GROWING DELIVERY NETWORK</button>
+</span>
+<img src={withYou} />
             </div>
         </div>
     );
 }
+
+const DeliverCard=(props)=>{
+    return (
+        <div className='border-[1px] p-2 inline-flex gap-2 items-center w-[80%] rounded-md'>
+            <img src={checkMark} className='w-[40px h-[40px]'/>
+            <h1 className='text-[22px]'>
+                {props.title}
+            </h1>
+        </div>
+    )
+}
+
+function RiderCard(props){
+    return (
+        <div className='border-[1px] rounded-md w-[300px] p-2 flex flex-col gap-2'>
+            <img src={props.Imageurl} className='w-[40px]'/>
+            <h1 className='text-[20px]'>
+                {props.title}
+            </h1>
+        </div>
+    )
+}
+function SectionCard(props){
+    return (
+        <div className='flex flex-row gap-2 border-[1px] p-2 rounded-md'>
+            <img src={checkMark} className='w-[30px]'/>
+            <h1 className='text-[20px]'>
+                {props.title}
+            </h1>
+        </div>
+    )	
+}
+
  
 export default Riders;
